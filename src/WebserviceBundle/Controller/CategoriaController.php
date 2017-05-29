3c249c7ff36109f73b921915e6d2ab11715e48cd
Marcos Alfredo Choque Asto <i201421104@cibertec.edu.pe>
2017-05-28 22:07:13 -0500

    categoria controller bin/console debug:router

--- src/WebserviceBundle/Controller/CategoriaController.php
+++ src/WebserviceBundle/Controller/CategoriaController.php
@@ -5,6 +5,20 @@ namespace WebserviceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use FOS\RestBundle\Controller\Annotations\View;
+
 class CategoriaController extends Controller
 {
+
+    /**
+     * @return array
+     * @View()
+     */
+    public function getCategoriasAction(){
+        
+        $categorias = $this->getDoctrine()
+            ->getRepository('AppRestWebserviceBundle:Categoria')
+            ->findAll();
+        return array('categorias' => $categorias);
+    }
 }
