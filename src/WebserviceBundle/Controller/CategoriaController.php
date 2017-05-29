298c905fc6987722b5341284d847c63f768784f5
Marcos Alfredo Choque Asto <i201421104@cibertec.edu.pe>
2017-05-28 22:15:56 -0500

    Categoria Controller con notaciones

--- src/WebserviceBundle/Controller/CategoriaController.php
+++ src/WebserviceBundle/Controller/CategoriaController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 use FOS\RestBundle\Controller\Annotations\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use WebserviceBundle\Entity\Categoria;
 
 class CategoriaController extends Controller
 {
@@ -20,5 +22,17 @@ class CategoriaController extends Controller
             ->getRepository('AppRestWebserviceBundle:Categoria')
             ->findAll();
         return array('categorias' => $categorias);
+    }
+
+
+    /**
+     * @param Categoria $categoria
+     * @return array
+     * @View()
+     * @ParamConverter("categoria", class="WebserviceBundle:Categoria")
+     */
+    public function getCategoriaAction(Categoria $categoria){
+
+        return array('categoria' => $categoria);
     }
 }
--- src/WebserviceBundle/Controller/DefaultController.php
+++ src/WebserviceBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace WebserviceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+
 class DefaultController extends Controller
 {
     /**
