1b279afd89e5a04b93df36be4ca7e920a72babad
Marcos Alfredo Choque Asto <i201421104@cibertec.edu.pe>
2017-05-28 21:24:12 -0500

    fos_rest... sensio_framework_extra...

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,10 +16,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-
+            new WebserviceBundle\WebserviceBundle(),
+            
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
-            new WebserviceBundle\WebserviceBundle(),
+            
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
