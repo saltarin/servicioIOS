f6aa98f693c3bfa635ee045377216467722dd2b9
Marcos Alfredo Choque Asto <i201421104@cibertec.edu.pe>
2017-06-15 01:59:07 -0500

    Oferta al 80% y comentario 25%

--- src/WebserviceBundle/Controller/ComentarioController.php
+++ src/WebserviceBundle/Controller/ComentarioController.php
@@ -5,10 +5,39 @@ namespace WebserviceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-use \FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use \FOS\RestBundle\Controller\FOSRestController;
+use \WebserviceBundle\Entity\Comentario;
+use \WebserviceBundle\Entity\Oferta;
+use \WebserviceBundle\Entity\Usuario;
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\Response;
 
 class ComentarioController extends FOSRestController
 {
+    /**
+     * @param \Oferta $oferta
+     * @return \array
+     * @Rest\View()
+     * @ParamConverter(name = "oferta", class= "WebserviceBundle:Oferta")
+     */
+    public function getComentariosAction(Oferta $oferta){
+
+        if($oferta == null){
+
+            return array('result' => 'no comentarios for oferta','code'=> Response::HTTP_NOT_FOUND);
+        }
+        
+        $comentarios = $this->getDoctrine()
+                        ->getRepository("WebserviceBundle:Comentario")
+                        ->findBy(array('oferta' => $oferta,'referencia' => null));
+        
+        if($comentarios == null){
+
+            return array('result' => 'no comentarios found', 'code' => Response::HTTP_NOT_FOUND);
+        }
+
+        return array('comentarios' => $comentarios, 'code' => Response::HTTP_OK);
+    }
 }
--- src/WebserviceBundle/Controller/OfertaController.php
+++ src/WebserviceBundle/Controller/OfertaController.php
@@ -5,10 +5,163 @@ namespace WebserviceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-use \FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use \FOS\RestBundle\Controller\FOSRestController;
+use WebserviceBundle\Entity\Oferta;
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\Response;
+use \WebserviceBundle\Entity\Categoria;
+use \WebserviceBundle\Entity\Captura;
+use \Doctrine\Common\Collections\ArrayCollection;
 
 class OfertaController extends FOSRestController
 {
+
+    /**
+     * @return array
+     * @Rest\View()
+     */
+    public function getOfertasAction(){
+
+        $ofertas = $this->getDoctrine()
+                        ->getRepository('WebserviceBundle:Oferta')
+                        ->findAll();
+        if($ofertas === null){
+
+            return(array('response' => 'no ofertas to show', 'code' => Response::HTTP_NOT_FOUND));
+        }
+        return array('ofertas' => $ofertas, 'code' => Response::HTTP_OK);
+    }
+
+    /**
+     * @param Oferta $oferta
+     * @return \array
+     * @Rest\View()
+     * @ParamConverter(name="oferta", class="WebserviceBundle:Oferta")
+     */
+    public function getOfertaAction(Oferta $oferta){
+
+        if($oferta === null){
+            return array('response' => 'no oferta found', 'code' => Response::HTTP_NOT_FOUND);
+        }
+
+        return array('oferta' => $oferta,'code' => Response::HTTP_OK);
+    }
+
+    /**
+     * @param Request $request
+     * @return \array
+     * @Rest\Post()
+     */
+    public function postOfertaAction(Request $request){
+
+        /*
+        $old_oferta = $this->getDoctrine()
+                        ->getRepository('WebserviceBundle:Oferta')
+                        ->find($request->get('id'));
+        
+        if($old_oferta != null){
+
+            return array('response' => 'Oferta exists','code' => Response::HTTP_NOT_ACCEPTABLE);
+        }
+        */
+
+        $oferta = new Oferta();
+        $oferta->setTitulo($request->get('titulo'));
+        $oferta->setDescripcion($request->get('descripcion'));
+        $oferta->setPrecio($request->get('precio'));
+        $oferta->setDescuento($request->get('descuento'));
+        $oferta->setTipoOferta($request->get('tipoOferta'));
+        $oferta->setPosX($request->get('posx'));
+        $oferta->setPosY($request->get('posy'));
+
+        //id categoria
+        $categoria =  $this->getDoctrine()
+                        ->getRepository('WebserviceBundle:Categoria')
+                        ->find($request->get('categoria'));
+        
+        //leer capturas for
+            #FALTA
+        //  \file $request->files->get('img')
+
+        $captura1 = new Captura();
+        $captura1->setUrl('img/vapenation.jpg');
+        $oferta->getCapturas()->add($captura1);
+        $this->getDoctrine()->getEntityManager()->persist($captura1);
+
+        $captura2 = new Captura();
+        $captura2->setUrl('img/vapenation.jpg');
+        $oferta->getCapturas()->add($captura2);
+        $this->getDoctrine()->getEntityManager()->persist($captura2);
+
+        $captura3 = new Captura();
+        $captura3->setUrl('img/vapenation.jpg');
+        $oferta->getCapturas()->add($captura3);
+        $this->getDoctrine()->getEntityManager()->persist($captura3);
+
+        //id usuario
+        $usuario = $this->getDoctrine()
+                        ->getRepository('WebserviceBundle:Usuario')
+                        ->find($request->get('usuario'));
+
+        $oferta->setCategoria($categoria);
+        $oferta->setUsuario($usuario);
+
+        $oferta->setEstado(1);
+        $oferta->setFechaCreacion(new \Datetime());
+
+        $this->getDoctrine()->getEntityManager()->persist($oferta);
+        $this->getDoctrine()->getEntityManager()->flush();
+
+        return array('oferta' => $oferta,'code' => Response::HTTP_OK);
+        
+    }
+
+    /**
+     * @param int $id
+     * @return \array
+     * @Rest\Put()
+     */
+    public function putOfertaAction($id, Request $request){
+    
+        $old_oferta = $this->getDoctrine()
+                        ->getRepository("WebserviceBundle:Oferta")
+                        ->find($id);
+
+        if($old_oferta == null){
+
+            return array('response' => 'oferta not found','code' => Response::HTTP_NOT_FOUND);
+        }
+
+        $old_oferta->setTitulo($request->get('titulo'));
+        $old_oferta->setDescripcion($request->get('descripcion'));
+        $old_oferta->setPrecio($request->get('precio'));
+        $old_oferta->setDescuento($request->get('descuento'));
+        $old_oferta->setTipoOferta($request->get('tipoOferta'));
+        $old_oferta->setPosX($request->get('posx'));
+        $old_oferta->setPosY($request->get('posy'));
+
+        //id categoria
+        $categoria =  $this->getDoctrine()
+                        ->getRepository('WebserviceBundle:Categoria')
+                        ->find((int)$request->get('categoria'));
+
+        //id usuario
+        $usuario = $this->getDoctrine()
+                        ->getRepository('WebserviceBundle:Usuario')
+                        ->find((int)$request->get('usuario'));
+
+        //falta tags
+
+        //falta reemplazar img
+        //$oferta->setAvatar($this->getRequest()->getUriFromPath('/img/vapenation.jpg'));
+        
+        $old_oferta->setEstado((int)$request->get('estado'));
+        $old_oferta->setFechaCreacion(new \Datetime($request->get('fechaRegistro')));
+
+        $this->getDoctrine()->getEntityManager()->flush();
+        
+        return array('oferta' => $old_oferta,'code' => Response::HTTP_OK);
+    }
 }
