0228b10e15dd84d501ec36f2cb348014bb8e0049
Marcos Alfredo Choque Asto <i201421104@cibertec.edu.pe>
2017-07-09 17:34:29 -0500

    Captura controller

--- src/WebserviceBundle/Controller/CapturaController.php
+++ src/WebserviceBundle/Controller/CapturaController.php
@@ -14,7 +14,62 @@ use \Symfony\Component\HttpFoundation\Response;
 use WebserviceBundle\Entity\Captura;
 use WebserviceBundle\Entity\Oferta;
 
-class CapturaController extends Controller
+class CapturaController extends FOSRestController
 {
+    /**
+     * @param Request $request
+     * @return array
+     * @Rest\Post()
+     */
+    public function findCapturaAction(Request $request){
 
+        $id_oferta = $request->get('oferta');
+
+        $oferta_old = $this->getDoctrine()
+                        ->getRepository('WebserviceBundle:Oferta')
+                        ->find($id_oferta);
+
+        if($oferta_old == null){
+
+            return array('code' => Response::HTTP_NOT_FOUND, 'response' => 'oferta not exists you fucking disgrace');
+        }
+
+        $capturas = $this->getDoctrine()
+                        ->getRepository('WebserviceBundle:Captura')
+                        ->findBy(array('oferta' => $oferta_old));
+
+        if($capturas == null){
+            
+            return array('response' => "Sin resultados", Response::HTTP_NOT_FOUND);
+        }
+        
+        return array('categorias' => $capturas, 'code' => Response::HTTP_OK);
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     * @Rest\Post()
+     */
+    public function postCapturaAction(Request $request){
+
+        $oferta_id = $request->get('oferta');
+        $url = $request->get('url');
+        
+        $oferta_old = $this->getDoctrine()
+                        ->getRepository('WebserviceBundle:Oferta')
+                        ->find($oferta_id);
+        
+        if($oferta_old == null){
+
+            return array('code' => Response::HTTP_NOT_FOUND, 'response' => 'oferta not found you stupid ass');
+        }
+
+        $captura = new Captura();
+        $captura->setUrl($url)->setOferta($oferta_old);
+        $this->getDoctrine()->getEntityManager()->persist($captura);
+        $this->getDoctrine()->getEntityManager()->flush();
+
+        return array('code' => Response::HTTP_OK, 'captura' => $captura);
+    }
 }
